33728221f403c45d6eb057a42d43af36f4101470
Julian Javillo <jjavillo@nexusmc200.umiacs.umd.edu>
2024-09-19 13:16:48 -0400

    removed inputs

--- ebdd_cluster/submit_ebdd_cluster.sh
+++ ebdd_cluster/submit_ebdd_cluster.sh
@@ -1,27 +1,29 @@
 #!/bin/bash -l
 
 # Give the job a name
-#SBATCH --job-name="rlwe_ebdd"
+#SBATCH --job-name="rlwe128_ebdd"
+#SBATCH --account=mc2
+#SBATCH --partition=mc2
 
 # Set the number of nodes (Physical Computers)
 #SBATCH --nodes=1
 
 # Set the number of cores needed
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=16
 
 # Set the amount of memory required
-#SBATCH --mem-per-cpu=2gb
+#SBATCH --mem-per-cpu=8gb
 
 # Set stdout/stderr
 #SBATCH --output="rlwe_ebdd.out.%j"
 #SBATCH --error="rlwe_ebdd.err.%j"
 
 # Set expected wall time for the job (format = hh:mm:ss)
-#SBATCH --time=18:00:00
+#SBATCH --time=24:00:00
 
 # Set quality of service level (useful for obtaining GPU resources)
-#SBATCH --qos=default
+#SBATCH --qos=HIGH
 
 # Turn on mail notifications for job failure and completion
 #SBATCH --mail-type=END,FAIL
@@ -42,17 +44,17 @@ mkdir -p ${SCRATCH_DIRECTORY}
 cd ${SCRATCH_DIRECTORY}
 
 # Copy code to the scratch directory
-cp -r ${SLURM_SUBMIT_DIR}/geometricLWE ${SCRATCH_DIRECTORY}
+cp -r ~/geometricLWE ${SCRATCH_DIRECTORY}
 
 # Run code
 
 OUTFILE=ebdd_cluster-${SLURM_JOBID}.log
-RESULTS=ebdd_cluster.csv
+RESULTS=ebdd_cluster-${SLURM_JOBID}.csv
 
 cd geometricLWE/ebdd_cluster
 sage --pip install --upgrade pip
 sage --pip install pandas
-sage ebdd_cluster.sage 100 1 > ${OUTFILE}
+sage ebdd_cluster.sage > ${OUTFILE}
 
 # Copy outputs back to home directory
 cp ${OUTFILE} ${SLURM_SUBMIT_DIR}
